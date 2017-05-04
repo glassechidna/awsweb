0a575e8ba7ef570db581aa39ac323ce843d81e03
Aidan Steele <aidan.steele@glassechidna.com.au>
2017-05-04 20:23:58 +1000

    Set default session duration to one hour (see #11)

--- shared/creds.go
+++ shared/creds.go
@@ -5,9 +5,12 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/pquerna/otp/totp"
+	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
 )
 
 func GetCreds(profile string, mfaSecret string) (credentials.Value, string) {
+	stscreds.DefaultDuration = 3600 // TODO: issue #11 make this configurable
+
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState:       session.SharedConfigEnable,
 		Profile:                 profile,
