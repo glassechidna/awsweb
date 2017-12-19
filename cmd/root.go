381398fa7f0a92c209e685ac4a008c5b85a0b4ea
Aidan Steele <aidan.steele@glassechidna.com.au>
2017-12-19 21:58:28 +1100

    Don't spam the terminal

--- cmd/root.go
+++ cmd/root.go
@@ -61,6 +61,5 @@ func initConfig() {
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	}
 }
